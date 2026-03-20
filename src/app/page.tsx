611cd1a6adc0b2a4f7c0f29515be6aae8ead2900
Tushar3232 <anikdebnath916@gmail.com>
2026-03-20 23:40:44 +0600

    Creating User, Admin, DeliveryBoy Dashboard component's and created logic in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,35 @@ import User from './models/user.model';
 import EditRoleMobile from '@/components/EditRoleMobile/EditRoleMobile';
 import { redirect } from 'next/navigation';
 import Nav from '@/components/navber/Nav';
+import UserDashboard from '@/components/userDashboard/UserDashboard';
+import AdminDashboard from '@/components/adminDashboard/AdminDashboard';
+import DeliveryBoyDashboard from '@/components/deliveryBoyDashboard/DeliveryBoyDashboard';
 
 const Home = async () => {
   await connectDb()
   const session = await auth()
-  console.log("Backend Session",session)
- 
+  console.log("Backend Session", session)
+
   const user = await User.findById(session?.user?.id)
-  if(!user){
+  if (!user) {
     redirect("/login")
   }
-  const inComplete= !user.mobile || !user.role || (!user.mobile && user.role == "user")
-  if(inComplete){
+  const inComplete = !user.mobile || !user.role || (!user.mobile && user.role == "user")
+  if (inComplete) {
     return <EditRoleMobile></EditRoleMobile>
   }
 
-  const plainUser=  JSON.parse(JSON.stringify(user))
+  const plainUser = JSON.parse(JSON.stringify(user))
   return (
     <>
       <Nav user={plainUser}></Nav>
+      {
+        user.role == "user" ? (<UserDashboard />)
+          :
+          user.role == "admin" ? (<AdminDashboard />)
+            :
+            <DeliveryBoyDashboard />
+      }
     </>
   );
 };
---
+++ src/components/deliveryBoyDashboard/DeliveryBoyDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeliveryBoyDashboard = () => {
+    return (
+        <div>
+            Delivery Boy Dashboard 
+        </div>
+    );
+};
+
+export default DeliveryBoyDashboard;
