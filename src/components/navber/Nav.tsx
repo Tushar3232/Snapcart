01118caa934680396cebfa1681d31e5557ee6022
Tushar3232 <anikdebnath916@gmail.com>
2026-03-22 23:35:11 +0600

    user nav component hide for others role

--- src/components/navber/Nav.tsx
+++ src/components/navber/Nav.tsx
@@ -29,33 +29,40 @@ const Nav = ({ user }: { user: IUser }) => {
             </Link>
 
             {/* search  */}
-            <form className=' hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md'>
-                <Search className=' text-gray-500 w-5 h-5 mr-2'></Search>
+            {
+                user?.role == "user" && <form className=' hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md'>
+                    <Search className=' text-gray-500 w-5 h-5 mr-2'></Search>
 
 
-                <input
-                    type="text"
-                    placeholder='Search groceries...'
-                    className='w-full outline-none text-gray-700 placeholder-gray-400'
-                />
+                    <input
+                        type="text"
+                        placeholder='Search groceries...'
+                        className='w-full outline-none text-gray-700 placeholder-gray-400'
+                    />
 
 
-            </form>
+                </form>
+            }
 
             <div className=' flex items-center gap-3 md:gap-6 relative'>
-                <div
-                    onClick={() => setSearchBarOpen((prev) => !prev)}
-                    className=' cursor-pointer bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden'>
-                    <Search />
-                </div>
+                {
+                    user?.role == "user" && <>
+                        <div
+                            onClick={() => setSearchBarOpen((prev) => !prev)}
+                            className=' cursor-pointer bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden'>
+                            <Search />
+                        </div>
 
 
-                <Link href={""}
-                    className=' relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'
-                >
-                    <ShoppingCartIcon className=' text-green-600 w-6 h-6' />
-                    <span className=' absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow'>0</span>
-                </Link>
+                        <Link href={""}
+                            className=' relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition'
+                        >
+                            <ShoppingCartIcon className=' text-green-600 w-6 h-6' />
+                            <span className=' absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow'>0</span>
+                        </Link>
+                    </>
+                }
+
                 <div className='relative'>
                     <div
                         onClick={() => setOpen(prev => !prev)}
@@ -130,17 +137,17 @@ const Nav = ({ user }: { user: IUser }) => {
                                 exit={{ opacity: 0, y: -10, scale: 0.95 }}
                                 className=' fixed top-24 left-1/2 -translate-x-1/2 w-[90%] bg-white rounded-full shadow-lg z-40 flex items-center px-4 py-2'
                             >
-                                <Search className=' text-gray-500 w-5 h-5 mr-2'/>
+                                <Search className=' text-gray-500 w-5 h-5 mr-2' />
                                 <form className=' grow' >
-                                    <input type="text" 
-                                    className=' w-full outline-none'
-                                    placeholder='Search groceries....'
+                                    <input type="text"
+                                        className=' w-full outline-none'
+                                        placeholder='Search groceries....'
                                     />
                                 </form>
-                                <button onClick={()=>setSearchBarOpen(false)}
+                                <button onClick={() => setSearchBarOpen(false)}
                                     className=' hover:text-red-600 cursor-pointer'
-                                    >
-                                    X 
+                                >
+                                    X
                                 </button>
                             </motion.div>
                         }
