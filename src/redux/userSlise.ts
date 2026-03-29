3cf7b7985500e4aa17b99894333784223b059d69
Tushar3232 <anikdebnath916@gmail.com>
2026-03-29 23:53:08 +0600

    Creating UserSlise logic

---
+++ src/redux/userSlise.ts
@@ -0,0 +1,24 @@
+import { IUser } from "@/app/models/user.model";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface IUserSlice{
+    userData: IUser | null
+}
+
+const initialState: IUserSlice={
+    userData:null
+}
+const userSlise= createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        setUserData:(state, action)=>{
+            state.userData=action.payload
+        }
+    }
+
+})
+
+export const {setUserData}=userSlise.actions
+export default userSlise.reducer
