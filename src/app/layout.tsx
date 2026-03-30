f9ebc431de143cef35a041c7203d46ef7ecef3f9
Tushar3232 <anikdebnath916@gmail.com>
2026-03-30 23:01:29 +0600

    Creating StoreProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Provider from "@/provider";
+import StoreProvider from "@/redux/StoreProvider";
 
 export const metadata: Metadata = {
   title: "Snapcart | 10 minutes grocery Delevry App",
@@ -16,7 +17,10 @@ export default function RootLayout({
     <html lang="en">
       <body className=" w-full min-h-screen bg-linear-to-b from-green-100 to-white">
         <Provider>
-          {children}
+          <StoreProvider>
+{children}
+          </StoreProvider>
+          
         </Provider>
       </body>
     </html>
