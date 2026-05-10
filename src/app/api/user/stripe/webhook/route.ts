27883bd21a040248063615ff570f25c0cb23b83f
Tushar3232 <anikdebnath916@gmail.com>
2026-05-10 17:13:20 +0600

    Add stripe webhook and complete stripe payment system

---
+++ src/app/api/user/stripe/webhook/route.ts
@@ -0,0 +1,40 @@
+import connectDb from "@/app/lib/db";
+import Order from "@/app/models/order.model";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
+export async function POST(req: NextRequest) {
+    const sig = req.headers.get("stripe-signature")
+    const rawBody = await req.text()
+    let event;
+    try {
+        event = Stripe.webhooks.constructEvent(
+            rawBody, sig!, process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch (error) {
+        console.log("signature verification failed", error)
+        return NextResponse.json(
+            { message: "Invalid signature" },
+            { status: 400 }
+        )
+    }
+
+    if (event?.type === "checkout.session.completed") {
+        const session = event.data.object
+        await connectDb()
+        await Order.findByIdAndUpdate(session.metadata?.orderId, {
+            isPaid: true
+        })
+
+        return NextResponse.json({ recived: true }, { status: 200 })
+
+    }
+
+    return NextResponse.json(
+        { received: true },
+        { status: 200 }
+    )
+
+}
